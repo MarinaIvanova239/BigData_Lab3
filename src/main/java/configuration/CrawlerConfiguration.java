841428b590ae93c6fd3c7290c7239f3a8d3394e8
MarinaIvanova239 <mar.ivanova@bk.ru>
2017-04-05 20:26:00 +0300

    add profiles and dependency

--- src/main/java/configuration/CrawlerConfiguration.java
+++ src/main/java/configuration/CrawlerConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.amqp.core.MessageListener;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("CRAWLER")
 public class CrawlerConfiguration {
 
     @Bean(name = "customListener")

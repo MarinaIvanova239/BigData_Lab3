44865b3bd8d0c64a628e4b22c5c9e4c809a8e7bb
MarinaIvanova239 <mar.ivanova@bk.ru>
2017-04-05 19:20:13 +0300

    small fix in rabbitmq config

--- src/main/java/common/InitialSender.java
+++ src/main/java/common/InitialSender.java
@@ -7,22 +7,19 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
+@Component
 @PropertySource("classpath:/../../resources/application.properties")
 public class InitialSender {
 
-    @Autowired
-    private AmqpAdmin rabbitAdmin;
     @Autowired
     private AmqpTemplate rabbitTemplate;
     @Autowired
     private Environment env;
 
     public void runInitial() throws Exception {
-        // declare queues
-        rabbitAdmin.declareQueue(new Queue("for_parsing"));
-        rabbitAdmin.declareQueue(new Queue("for_downloading"));
         // send initial message to queue for parsing
         String initialMessage = env.getProperty("rabbitmq.init_message");
         rabbitTemplate.send("for_parsing", new Message(initialMessage.getBytes(), null));

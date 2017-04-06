4ef188f79357ca356a0df52ef6bb79f7de1d6ce9
MarinaIvanova239 <mar.ivanova@bk.ru>
2017-04-06 21:04:19 +0300

    edit mongodb config

--- src/main/java/configuration/MongoConfiguration.java
+++ src/main/java/configuration/MongoConfiguration.java
@@ -1,12 +1,15 @@
 package configuration;
 
+import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -16,7 +19,7 @@ import java.io.IOException;
 @Configuration
 @EnableMongoRepositories
 @PropertySource("classpath:/../../resources/application.properties")
-public class MongoConfiguration {
+public class MongoConfiguration extends AbstractMongoConfiguration {
 
     @Autowired
     Environment env;
@@ -27,15 +30,31 @@ public class MongoConfiguration {
                 Integer.parseInt(env.getProperty("mongodb.port")));
     }
 
-    @Autowired
     @Bean(name="mongoDbFactory")
-    public MongoDbFactory mongoDbFactory(MongoClient mongoClient) {
-        return new SimpleMongoDbFactory(mongoClient, env.getProperty("mongodb.database"));
+    public MongoDbFactory mongoDbFactory() throws Exception {
+        UserCredentials userCredentials = new UserCredentials(env.getProperty("mongodb.admin"),
+                env.getProperty("mongodb.password"));
+        return new SimpleMongoDbFactory(new Mongo(), env.getProperty("mongodb.database"),
+                userCredentials);
     }
 
-    @Autowired
     @Bean(name="mongoTemplate")
-    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
-        return new MongoTemplate(mongoClient, env.getProperty("mongodb.database"));
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongoDbFactory());
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return env.getProperty("mongodb.database");
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new Mongo();
+    }
+
+    @Override
+    protected String getMappingBasePackage() {
+        return "database.repositories";
     }
 }
